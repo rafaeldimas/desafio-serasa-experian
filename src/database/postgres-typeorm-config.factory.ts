078fee7eb7fdd02c59bdcec5188e172ec937d340
Rafael Dimas <rafael.dimas@live.com>
2025-06-05 18:08:28 -0300

    Change name of folder with config database and added notation @Injectable in the factory of postgress configs

--- src/database/postgres-typeorm-config.factory.ts
+++ src/database/postgres-typeorm-config.factory.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 
+@Injectable()
 export class PostgresTypeormConfigFactory implements TypeOrmOptionsFactory {
   constructor(private readonly configService: ConfigService) {}
 
