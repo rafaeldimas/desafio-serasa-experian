6bcabd81edf3d56ac3b919c38cf2f07c27641a1c
Rafael Dimas <rafael.dimas@live.com>
2025-06-05 19:55:41 -0300

    change name of database folder to config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PostgresTypeormConfigFactory } from './database/postgres-typeorm-config.factory';
+import { PostgresTypeormConfigFactory } from './config/postgres-typeorm-config.factory';
 
 @Module({
   imports: [
