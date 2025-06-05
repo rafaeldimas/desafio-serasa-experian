fc6e7de7513d1da8afd4c31cc2e1389df69052d8
Rafael Dimas <rafael.dimas@live.com>
2025-06-05 16:49:30 -0300

    Added swagger and added initial configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Desafio Serasa Experian')
+    .setDescription(
+      'Documentação da API do desafio para a vaga de backend do Serasa Experian',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, { autoTagControllers: true });
+
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
